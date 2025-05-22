48374f4b3b459b224c47388d4536de1f7d2c2fa9
devZiyad <164162565+devZiyad@users.noreply.github.com>
2025-05-23 01:18:00 +0300

    change color scheme & update plugins

--- init.lua
+++ init.lua
@@ -740,20 +740,26 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'catppuccin/nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
+      vim.cmd.colorscheme 'catppuccin-mocha'
+
+      custom_highlights = function(colors)
+        return {
+          Comment = { fg = colors.flamingo },
+          TabLineSel = { bg = colors.pink },
+          CmpBorder = { fg = colors.surface2 },
+          Pmenu = { bg = colors.none },
+        }
+      end
+
+      require('catppuccin').setup {
+        integrations = {
+          mason = true,
+          blink_cmp = true,
         },
       }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 
